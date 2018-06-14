617a0eaf68d9e3ff87e4cc9746255125bd305562
CJuice <CJuice@users.noreply.github.com>
2018-06-14 13:13:23 -0400

    revisions to sql, and classes to reflect data changes
    Data header names have changed, as well as ordering.

--- MDElect_UpdateProcess.py
+++ MDElect_UpdateProcess.py
@@ -1,7 +1,7 @@
 """
 DRAFT - Combine data from two csv's and update data in AGOL hosted feature layer with the new combined dataset
 """
-#TODO: pain point, when need to add field etc have to manually change in multiple spots. Make code more flexible.
+#TODO: major weakness/pain point, when need to add/revise/delete field etc you have to manually change in multiple spots. Make code more flexible to solve this issue.
 
 # IMPORTS
 from collections import namedtuple
@@ -11,23 +11,23 @@ import sqlite3
 class Bridge_Class():
     def __init__(self, data_dict):
         self.row_id = data_dict["Row_ID"]
-        self.mddistrict = data_dict["MDDistrict"]
-        self.usdistrict = data_dict["USDistrict"]
+        self.md_district = data_dict["MD_District"]
+        self.us_district = data_dict["US_District"]
 class MDGov_Class():
     def __init__(self, data_dict):
-        self.district = data_dict["District"]
+        self.md_district = data_dict["MD_District"]
         self.state_senator = data_dict["State_Senator"]
-        self.state_representative_1 = data_dict["State_Representative_1"]
-        self.state_representative_2 = data_dict["State_Representative_2"]
-        self.state_representative_3 = data_dict["State_Representative_3"]
         self.state_senator_party = data_dict["State_Senator_Party"]
+        self.state_senator_maryland_manual_online = data_dict["State_Senator_Maryland_Manual_Online"]
+        self.state_representative_1 = data_dict["State_Representative_1"]
         self.state_representative_1_party = data_dict["State_Representative_1_Party"]
+        self.state_representative_1_maryland_manual_online = data_dict["State_Representative_1_Maryland_Manual_Online"]
+        self.state_representative_2 = data_dict["State_Representative_2"]
         self.state_representative_2_party = data_dict["State_Representative_2_Party"]
+        self.state_representative_2_maryland_manual_online = data_dict["State_Representative_2_Maryland_Manual_Online"]
+        self.state_representative_3 = data_dict["State_Representative_3"]
         self.state_representative_3_party = data_dict["State_Representative_3_Party"]
-        self.senator_maryland_manual_online = data_dict["Senator_Maryland_Manual_Online"]
-        self.representative_1_maryland_manual_online = data_dict["Representative_1_Maryland_Manual_Online"]
-        self.representative_2_maryland_manual_online = data_dict["Representative_2_Maryland_Manual_Online"]
-        self.representative_3_maryland_manual_online = data_dict["Representative_3_Maryland_Manual_Online"]
+        self.state_representative_3_maryland_manual_online = data_dict["State_Representative_3_Maryland_Manual_Online"]
         self.governor = data_dict["Governor"]
         self.governor_maryland_manual_online = data_dict["Governor_Maryland_Manual_Online"]
         self.lt_governor = data_dict["Lt_Governor"]
@@ -38,43 +38,43 @@ class MDGov_Class():
         self.comptroller_maryland_manual_online = data_dict["Comptroller_Maryland_Manual_Online"]
 class USGov_Class():
     def __init__(self, data_dict):
-        self.district = data_dict["District"]
+        self.us_district = data_dict["US_District"]
         self.name = data_dict["Name"]
         self.label = data_dict["Label"]
-        self.us_representatives = data_dict["US_Representatives"]
-        self.party = data_dict["Party"]
         self.us_senator_1 = data_dict["US_Senator_1"]
         self.us_senator_1_party = data_dict["US_Senator_1_Party"]
+        self.us_senator_1_maryland_manual_online = data_dict["US_Senator_1_Maryland_Manual_Online"]
         self.us_senator_2 = data_dict["US_Senator_2"]
         self.us_senator_2_party = data_dict["US_Senator_2_Party"]
-        self.us_senator_1_maryland_manual_online = data_dict["US_Senator_1_Maryland_Manual_Online"]
         self.us_senator_2_maryland_manual_online = data_dict["US_Senator_2_Maryland_Manual_Online"]
-        self.us_representative_maryland_manual_online = data_dict["US_Representative_Maryland_Manual_Online"]
+        self.us_representatives = data_dict["US_Representatives"]
+        self.us_representatives_party = data_dict["US_Representatives_Party"]
+        self.us_representatives_maryland_manual_online = data_dict["US_Representatives_Maryland_Manual_Online"]
 
 # VARIABLES - CONSTANTS
 Variable = namedtuple("Variable", "value")
 CSV_BRIDGE = Variable(r"Docs\20180613_Bridge.csv")
-CSV_MDGOV = Variable(r"Docs\20180613_ElectedOfficialsMarylandGovernment.csv")
-CSV_USGOV = Variable(r"Docs\20180613_ElectedOfficialsUSGovernment.csv")
+CSV_MDGOV = Variable(r"Docs\20180614_ElectedOfficialsMarylandGovernment.csv")
+CSV_USGOV = Variable(r"Docs\20180614_ElectedOfficialsUSGovernment.csv")
 SQL_CREATE_BRIDGE = Variable("""CREATE TABLE BRIDGE (
                     Row_ID text primary key, 
-                    MDDistrict text,
-                    USDistrict text                    
+                    MD_District text,
+                    US_District text                    
                     )""")
 SQL_CREATE_MDGOV = Variable("""CREATE TABLE MDGOV (
-                    District text primary key,
+                    MD_District text primary key,
                     State_Senator text,
-                    State_Representative_1 text,
-                    State_Representative_2 text,
-                    State_Representative_3 text,
                     State_Senator_Party text,
+                    State_Senator_Maryland_Manual_Online text,
+                    State_Representative_1 text,
+                    State_Representative_1_Maryland_Manual_Online text,
                     State_Representative_1_Party text,
+                    State_Representative_2 text,
                     State_Representative_2_Party text,
+                    State_Representative_2_Maryland_Manual_Online text,
+                    State_Representative_3 text,
                     State_Representative_3_Party text,
-                    Senator_Maryland_Manual_Online text,
-                    Representative_1_Maryland_Manual_Online text,
-                    Representative_2_Maryland_Manual_Online text,
-                    Representative_3_Maryland_Manual_Online text,
+                    State_Representative_3_Maryland_Manual_Online text,
                     Governor text,
                     Governor_Maryland_Manual_Online text,
                     Lt_Governor text,
@@ -85,23 +85,23 @@ SQL_CREATE_MDGOV = Variable("""CREATE TABLE MDGOV (
                     Comptroller_Maryland_Manual_Online text
                     )""")
 SQL_CREATE_USGOV = Variable("""CREATE TABLE USGOV (
-                    District text primary key,
+                    US_District text primary key,
                     Name integer,
                     Label text,
-                    US_Representatives text,
-                    Party text,
                     US_Senator_1 text,
                     US_Senator_1_Party text,
+                    US_Senator_1_Maryland_Manual_Online text,
                     US_Senator_2 text,
                     US_Senator_2_Party text,
-                    US_Senator_1_Maryland_Manual_Online text,
                     US_Senator_2_Maryland_Manual_Online text,
+                    US_Representatives text,
+                    US_Representatives_Party text,
                     US_Representative_Maryland_Manual_Online text
                     )""")
-SQL_INSERT_BRIDGE = Variable("""INSERT OR IGNORE INTO bridge VALUES (:row_id, :mddistrict, :usdistrict)""")
-SQL_INSERT_MDGOV = Variable("""INSERT OR IGNORE INTO mdgov VALUES (:district, :state_senator, :state_representative_1, :state_representative_2, :state_representative_3, :state_senator_party, :state_representative_1_party, :state_representative_2_party, :state_representative_3_party, :senator_maryland_manual_online, :representative_1_maryland_manual_online, :representative_2_maryland_manual_online, :representative_3_maryland_manual_online, :governor, :governor_maryland_manual_online, :lt_governor, :lt_governor_maryland_manual_online, :attorney_general, :attorney_general_maryland_manual_online, :comptroller, :comptroller_maryland_manual_online)""")
-SQL_INSERT_USGOV = Variable("""INSERT OR IGNORE INTO usgov VALUES (:district, :name, :label, :us_representatives, :party, :us_senator_1, :us_senator_1_party, :us_senator_2, :us_senator_2_party, :us_senator_1_maryland_manual_online, :us_senator_2_maryland_manual_online, :us_representative_maryland_manual_online)""")
-SQL_SELECT_OUTPUT_DATA = Variable("""SELECT MDGOV.*, BRIDGE.Row_ID, USGOV.* FROM MDGOV, BRIDGE, USGOV WHERE MDGOV.District = BRIDGE.MDDistrict AND BRIDGE.USDistrict = USGOV.District""")
+SQL_INSERT_BRIDGE = Variable("""INSERT OR IGNORE INTO bridge VALUES (:row_id, :md_district, :us_district)""")
+SQL_INSERT_MDGOV = Variable("""INSERT OR IGNORE INTO mdgov VALUES (:md_district, :state_senator, :state_senator_party, :state_senator_maryland_manual_online, :state_representative_1, :state_representative_1_party, :state_representative_1_maryland_manual_online, :state_representative_2, :state_representative_2_party, :state_representative_2_maryland_manual_online, :state_representative_3,  :state_representative_3_party, :state_representative_3_maryland_manual_online, :governor, :governor_maryland_manual_online, :lt_governor, :lt_governor_maryland_manual_online, :attorney_general, :attorney_general_maryland_manual_online, :comptroller, :comptroller_maryland_manual_online)""")
+SQL_INSERT_USGOV = Variable("""INSERT OR IGNORE INTO usgov VALUES (:us_district, :name, :label, :us_senator_1, :us_senator_1_party, :us_senator_1_maryland_manual_online, :us_senator_2, :us_senator_2_party, :us_senator_2_maryland_manual_online, :us_representatives, :us_representatives_party, :us_representatives_maryland_manual_online)""")
+SQL_SELECT_OUTPUT_DATA = Variable("""SELECT MDGOV.*, BRIDGE.Row_ID, USGOV.* FROM MDGOV, BRIDGE, USGOV WHERE MDGOV.MD_District = BRIDGE.MD_District AND BRIDGE.US_District = USGOV.US_District""")
 
 # VARIABLES - OTHER
 test_database = r"Docs\testdb.db"           # TESTING
@@ -132,8 +132,8 @@ def execute_sql_command(cursor, sql_command, parameters_sequence=()):
 
 # FUNCTIONALITY
 # Set up SQLite3 in memory database. Establish database tables
-# conn = create_database_connection(test_database)    # TESTING
-conn = create_database_connection(":memory:")
+conn = create_database_connection(test_database)    # TESTING
+# conn = create_database_connection(":memory:")
 curs = create_database_cursor(conn)
 for sql_namedtuple in sql_create_namedtuples_list:
     execute_sql_command(cursor=curs, sql_command=sql_namedtuple.value)
@@ -160,8 +160,8 @@ for csv_namedtuple in csv_namedtuples_list:
 results = execute_sql_command(cursor=curs,sql_command=SQL_SELECT_OUTPUT_DATA.value)
 
 # Make connection with AGOL
-# Update data in hosted feature layer
 
+# Update data in hosted feature layer
 
 # Commit and Close things out
 commit_to_database(conn)
